eb7ae2a2fe143a889849fd4f859d5efbb25fc7ff
Kenny Lazer <kennyruzindana10@gmail.com>
2020-09-07 13:01:00 +0200

    Styled my contacts page

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import '../assets/styles/myContacts.scss';
 import fetchContactsAction from '../redux/actions/contacts/fetchContacts';
 
 const Contacts = ({ fetchContactsAction, fetchContacts }) => {
@@ -30,34 +31,72 @@ const Contacts = ({ fetchContactsAction, fetchContacts }) => {
   // ));
   return (
     //Start
-    <>
-    {data.map((contact) => (
-      <div key={contact._id}>
-        <div className="container">
-  <table className="table table-dark">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Phone Number</th>
-        <th>Address</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-    <td>{contact.name}</td>
-    <td>{contact.phone_number}</td>
-    <td>{contact.address}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-<div className="button-section text-center">
-<button type="button" className="btn btn-success">Update</button>
-<button type="button" className="btn btn-warning">Delete</button>
-</div>
+    <div>
+      <h4 className="text-center text-light bg-dark mb-0 pb-1">
+        Contacts List
+      </h4>
+      <div className="table-1">
+        <table className="table table-dark">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Phone Number</th>
+              <th>Address</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((contact) => (
+              <tr>
+                <td>{contact.name}</td>
+                <td>{contact.phone_number}</td>
+                <td>{contact.address}</td>
+                <td>
+                  <button type="button" className="btn btn-warning">
+                    Update
+                  </button>
+                </td>
+                <td>
+                  <button type="button" className="btn btn-danger">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    ))}
-    </>
+    </div>
+
+    //     <>
+    //     {data.map((contact) => (
+    //       <div key={contact._id}>
+    //         <div className="container">
+    //   <table className="table table-dark">
+    //     <thead>
+    //       <tr>
+    //         <th>Name</th>
+    //         <th>Phone Number</th>
+    //         <th>Address</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       <tr>
+    //     <td>{contact.name}</td>
+    //     <td>{contact.phone_number}</td>
+    //     <td>{contact.address}</td>
+    //       </tr>
+    //     </tbody>
+    //   </table>
+    // </div>
+    // <div className="button-section text-center">
+    // <button type="button" className="btn btn-success">Update</button>
+    // <button type="button" className="btn btn-warning">Delete</button>
+    // </div>
+    //       </div>
+    //     ))}
+    //     </>
   ); // End of Return
 };
 
